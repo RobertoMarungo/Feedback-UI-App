79dd23fa1f313ea13e23f364ac92aef5fc06566e
Roberto Marungo <robmarungo.dev@gmail.com>
2024-05-19 03:05:40 -0500

    Added comments to code and radio butons to form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+//Imports
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
@@ -6,19 +7,25 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackForm from './components/FeedbackForm';
 
 function App() {
+  //UseState
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  //Function to add Feedback item
+  const addFeedback = (newFeedback) => {};
+
+  //Function to delete Feedback item
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
+  //App Component Return
   return (
     <>
       <Header />
       <div className='container'>
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList
           feedback={feedback}
